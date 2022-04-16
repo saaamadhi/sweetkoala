e910f4afa57b0ee03ec6e1515e225210fee4a202
Angelina Valer'eva <mandaaarinka@mail.ru>
2022-04-16 21:20:21 +0300

    feat(header): add redux, prop-types, update deps

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import './shared/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
